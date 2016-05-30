da1621abd36c14cf9c2b42c6578cc91aeac3b34b
Christian Fei <crifei93@gmail.com>
2016-05-30 16:17:34 +0200

    add label to display coordinates

--- app/app.js
+++ app/app.js
@@ -15,10 +15,28 @@ export class MyApp {
   constructor(platform) {
     this.rootPage = MainPage;
 
+    this.getPosition()
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
     });
+  }
+
+  getPosition() {
+    const self = this
+    let options = {timeout: 10000, enableHighAccuracy: true};
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        console.log('-- position.coords.latitude', position.coords.latitude)
+        console.log('-- position.coords.longitude', position.coords.longitude)
+        self.userPosition = position.coords
+      },
+      (error) => {
+        console.log(error);
+      },
+      options
+    );
   }
 }
