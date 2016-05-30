70decc241904b7c0938380a91ecf8f5bab454c8b
Christian Fei <crifei93@gmail.com>
2016-05-30 17:01:54 +0200

    show user coordinates

--- app/main.js
+++ app/main.js
@@ -6,5 +6,22 @@ import {Page} from 'ionic-angular';
 })
 export class MainPage {
   constructor() {
+    this.getPosition()
+  }
+
+  getPosition() {
+    const self = this
+    let options = {timeout: 10000, enableHighAccuracy: true};
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        console.log('-- position.coords.latitude', position.coords.latitude)
+        console.log('-- position.coords.longitude', position.coords.longitude)
+        self.userPosition = position.coords
+      },
+      (error) => {
+        console.log(error);
+      },
+      options
+    );
   }
 }
