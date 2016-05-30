c5f44c9a616e6aeebb80a092285c64dd04b455b2
Christian Fei <crifei93@gmail.com>
2016-05-30 15:16:18 +0200

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+# setup
+
+### requirements
+
+- node (preferably 5+)
+
+### install dependencies
+
+- `npm i -g ionic@beta cordova ios-sim ios-deploy`
---
+++ app/main.js
@@ -0,0 +1,10 @@
+import {Page} from 'ionic-angular';
+
+
+@Page({
+  templateUrl: 'build/main.html'
+})
+export class MainPage {
+  constructor() {
+  }
+}
